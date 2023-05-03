90b6f1723948b841a79f13379307283525364097
Ceeh-Jay <chiemekaelumeziem@gmail.com>
2023-05-03 13:39:34 +0100

    first commit

---
+++ routes/jobs.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+  getAllJobs,
+} = require("../controllers/jobs");
+
+
+router.route("/").get(getAllJobs).post(createJob);
+router.route("/:id").get(getJob).delete(deleteJob).patch(updateJob);
+
+module.exports = router;
+
